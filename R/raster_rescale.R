5d57ef277b568e4a8891d4ba8e1b1dd6dd098fdd
jngtz <jason.n.goetz@gmail.com>
2020-10-06 14:13:57 +0200

    update

---
+++ R/raster_rescale.R
@@ -0,0 +1,35 @@
+#' Cumulative Distribution Function for Rasters
+#'
+#' Compute an empirical cumulative distribution function for a raster.
+#' @param x A RasterLayer object
+#' @return RasterLayer
+#' @examples
+#' r <- raster(system.file("external/test.grd", package="raster"))
+#' r_ecdf <- RasterCdf(r)
+#' plot(r_ecdf)
+
+RasterCdf <- function(x){
+
+  x_ecdf <- ecdf(raster::getValues(x))
+  prob_x <- raster::setValues(x, x_ecdf(raster::getValues(x)))
+  prob_x
+
+}
+
+
+
+#' Min-max normalization (Rescaling)
+#'
+#' Rescale a raster values in the range from 0 to 1 using min-max normalization.
+#' @param x A RasterLayer object
+#' @return RasterLayer
+#' @examples
+#' r <- raster(system.file("external/test.grd", package="raster"))
+#' r_minmax <- RasterMinMaxRescale(r)
+#' plot(r_minmax)
+
+RasterMinMaxRescale <- function(x){
+
+  ( x - min(raster::getValues(x), na.rm=TRUE)) / (max(raster::getValues(x), na.rm=TRUE) -  min(raster::getValues(x), na.rm=TRUE) )
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# runout.opt
-An R package for optimizing mass movement runout models
